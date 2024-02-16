f589ac302a535a7493a5b05e20fa1fa3c0bc4872
Anna Yurevych <annayurevych@gmail.com>
2024-02-16 15:46:53 +0200

    Stage1.5

---
+++ MusicServiceInfrastructure/Controllers/ChartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicServiceInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly DbsongsContext _context;
+        public ChartController(DbsongsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var genres = _context.Genres.ToList();
+            List<object> catSong = new List<object>();
+            catSong.Add(new[] { "Жанр", "Кількість пісень" });
+            foreach (var c in genres)
+            {
+                catSong.Add(new object[] { c.Name, c.SongsGenres.Count() });
+            }
+            return new JsonResult(catSong);
+        }
+    }
+}
